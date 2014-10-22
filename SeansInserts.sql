e838b8a5c11152513b7481969a85eb7ad5fb0abf
sharto66 <c12425762@mydit.ie>
2014-10-22 18:22:38 +0100

    car insert statements added by Sean

---
+++ SeansInserts.sql
@@ -0,0 +1,7 @@
+INSERT INTO Car(Registration, Make, Year_of_Manufacture, OwnerID) VALUES('12-D-12345', 'BMW', DATE(2012-10-11), 100);
+INSERT INTO Car(Registration, Make, Year_of_Manufacture, OwnerID) VALUES('12-D-51234', 'Mercedes', DATE(2012-08-12), 101);
+INSERT INTO Car(Registration, Make, Year_of_Manufacture, OwnerID) VALUES('12-W-45123', 'BMW', DATE(2012-09-13), 102);
+INSERT INTO Car(Registration, Make, Year_of_Manufacture, OwnerID) VALUES('08-KE-34512', 'Mercedes', DATE(2008-08-12), 103);
+INSERT INTO Car(Registration, Make, Year_of_Manufacture, OwnerID) VALUES('90-D-23451', 'Porsche', DATE(1990-03-10), 104);
+INSERT INTO Car(Registration, Make, Year_of_Manufacture, OwnerID) VALUES('05-D-23456', 'BMW', DATE(2005-04-30), 105);
+INSERT INTO Car(Registration, Make, Year_of_Manufacture, OwnerID) VALUES('09-C-62345', 'Ferrari', DATE(2009-10-26), 106);
