991e2867e7624966daa475219d70ba0d569b94a3
sharto66 <c12425762@mydit.ie>
2014-10-22 19:17:07 +0100

    more insert statements added by Sean. Look at the file to findout which

--- SeansInserts.sql
+++ SeansInserts.sql
@@ -1,17 +1,31 @@
-INSERT INTO Car(Registration, Make, Year_of_Manufacture, OwnerID) VALUES('12-D-12345', 'BMW', DATE(2012-10-11), 100);
-INSERT INTO Car(Registration, Make, Year_of_Manufacture, OwnerID) VALUES('06-D-51234', 'Mercedes', DATE(2012-08-12), 101);
-INSERT INTO Car(Registration, Make, Year_of_Manufacture, OwnerID) VALUES('07-W-45123', 'BMW', DATE(2012-09-12), 102);
-INSERT INTO Car(Registration, Make, Year_of_Manufacture, OwnerID) VALUES('08-KE-34512', 'Mercedes', DATE(2008-08-12), 103);
-INSERT INTO Car(Registration, Make, Year_of_Manufacture, OwnerID) VALUES('90-D-23451', 'Porsche', DATE(1990-03-10), 104);
-INSERT INTO Car(Registration, Make, Year_of_Manufacture, OwnerID) VALUES('05-D-23456', 'BMW', DATE(2005-04-11), 105);
-INSERT INTO Car(Registration, Make, Year_of_Manufacture, OwnerID) VALUES('09-C-62345', 'Ferrari', DATE(2009-10-05), 106);
+INSERT INTO Car(Registration, Make, Year_of_Manufacture, OwnerID) VALUES('12-D-12345', 'BMW', TO_DATE('2012-10-11', 'YYYY-MM-DD'), 100);
+INSERT INTO Car(Registration, Make, Year_of_Manufacture, OwnerID) VALUES('06-D-51234', 'Mercedes', TO_DATE('2012-08-12', 'YYYY-MM-DD'), 101);
+INSERT INTO Car(Registration, Make, Year_of_Manufacture, OwnerID) VALUES('07-W-45123', 'BMW', TO_DATE('2012-09-12', 'YYYY-MM-DD'), 102);
+INSERT INTO Car(Registration, Make, Year_of_Manufacture, OwnerID) VALUES('08-KE-34512', 'Mercedes', TO_DATE('2008-08-12', 'YYYY-MM-DD'), 103);
+INSERT INTO Car(Registration, Make, Year_of_Manufacture, OwnerID) VALUES('90-D-23451', 'Porsche', TO_DATE('1990-03-10', 'YYYY-MM-DD'), 104);
+INSERT INTO Car(Registration, Make, Year_of_Manufacture, OwnerID) VALUES('05-D-23456', 'BMW', TO_DATE('2005-04-11', 'YYYY-MM-DD'), 105);
+INSERT INTO Car(Registration, Make, Year_of_Manufacture, OwnerID) VALUES('09-C-62345', 'Ferrari', TO_DATE('2009-10-05', 'YYYY-MM-DD'), 106);
 
-INSERT INTO CarRecord(Registration, Date_of_Last_Test, No._of_Fails, Is_Destroyed) VALUES('12-D-12345', DATE(2013-03-01), 0, FALSE);
-INSERT INTO CarRecord(Registration, Date_of_Last_Test, No._of_Fails, Is_Destroyed) VALUES('06-D-51234', DATE(2010-06-09), 1, FALSE);
-INSERT INTO CarRecord(Registration, Date_of_Last_Test, No._of_Fails, Is_Destroyed) VALUES('07-W-45123', DATE(2008-02-05), 0, FALSE);
-INSERT INTO CarRecord(Registration, Date_of_Last_Test, No._of_Fails, Is_Destroyed) VALUES('08-KE-34512', DATE(2009-10-04), 0, FALSE);
-INSERT INTO CarRecord(Registration, Date_of_Last_Test, No._of_Fails, Is_Destroyed) VALUES('90-D-23451', DATE(2014-02-02), 2, FALSE);
-INSERT INTO CarRecord(Registration, Date_of_Last_Test, No._of_Fails, Is_Destroyed) VALUES('05-D-23456', DATE(2010-01-01), 1, FALSE);
-INSERT INTO CarRecord(Registration, Date_of_Last_Test, No._of_Fails, Is_Destroyed) VALUES('09-C-62345', DATE(2011-02-09), 3, TRUE);
+INSERT INTO CarRecord(Registration, Date_of_Last_Test, No._of_Fails, Is_Destroyed) VALUES('12-D-12345', TO_DATE('2013-03-01', 'YYYY-MM-DD'), 0, FALSE);
+INSERT INTO CarRecord(Registration, Date_of_Last_Test, No._of_Fails, Is_Destroyed) VALUES('06-D-51234', TO_DATE('2010-06-09', 'YYYY-MM-DD'), 1, FALSE);
+INSERT INTO CarRecord(Registration, Date_of_Last_Test, No._of_Fails, Is_Destroyed) VALUES('07-W-45123', TO_DATE('2008-02-05', 'YYYY-MM-DD'), 0, FALSE);
+INSERT INTO CarRecord(Registration, Date_of_Last_Test, No._of_Fails, Is_Destroyed) VALUES('08-KE-34512', TO_DATE('2009-10-04', 'YYYY-MM-DD'), 0, FALSE);
+INSERT INTO CarRecord(Registration, Date_of_Last_Test, No._of_Fails, Is_Destroyed) VALUES('90-D-23451', TO_DATE('2014-02-02', 'YYYY-MM-DD'), 2, FALSE);
+INSERT INTO CarRecord(Registration, Date_of_Last_Test, No._of_Fails, Is_Destroyed) VALUES('05-D-23456', TO_DATE('2010-01-01', 'YYYY-MM-DD'), 1, FALSE);
+INSERT INTO CarRecord(Registration, Date_of_Last_Test, No._of_Fails, Is_Destroyed) VALUES('09-C-62345', TO_DATE('2011-02-09', 'YYYY-MM-DD'), 3, TRUE);
 
-INSERT INTO Letters(Registration, Test_No., TestPassed, Coment, TestDate, CentreName) VALUES( , 
+INSERT INTO Supervisor(SupervisorName, SupervisorID) VALUES('Kurt Russell', 100);
+INSERT INTO Supervisor(SupervisorName, SupervisorID) VALUES('Nigel Delaney', 101);
+
+INSERT INTO TestCentre(CentreName, Reigion, SupervisorID) VALUES('Rathgar Centre', 'Dublin South', 100);
+INSERT INTO TestCentre(CentreName, Reigion, SupervisorID) VALUES('Swords Centre', 'Dublin North', 101);
+
+INSERT INTO ClericalOfficer(ClerkName, CentreName) VALUES('Brendan Powers', 'Rathgar Centre');
+INSERT INTO ClericalOfficer(ClerkName, CentreName) VALUES('Jimmy Crocket', 'Rathgar Centre');
+INSERT INTO ClericalOfficer(ClerkName, CentreName) VALUES('Pepe Hennesey', 'Swords Centre');
+INSERT INTO ClericalOfficer(ClerkName, CentreName) VALUES('Mike Hunt', 'Swords Centre');
+
+INSERT INTO Letters(Registration, Date, ClerkName, Name, Address, OwnerID) VALUES('06-D-51234', TO_DATE('2010-05-09', 'YYYY-MM-DD'), , , ,)
+INSERT INTO Letters(Registration, Date, ClerkName, Name, Address, OwnerID) VALUES(, TO_DATE('2010-05-09', 'YYYY-MM-DD'), , , ,)
+INSERT INTO Letters(Registration, Date, ClerkName, Name, Address, OwnerID) VALUES(, TO_DATE('2010-05-09', 'YYYY-MM-DD'), , , ,)
+INSERT INTO Letters(Registration, Date, ClerkName, Name, Address, OwnerID) VALUES(, TO_DATE('2010-05-09', 'YYYY-MM-DD'), , , ,)
