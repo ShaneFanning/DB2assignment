0c03b9664bf5bff8872085af6b512de16e2a7f71
Mark <C12474988@mydit.ie>
2014-10-23 21:54:20 +0100

    Added the all inserts to the one file

--- SeansInserts.sql
+++ SeansInserts.sql
@@ -76,5 +76,32 @@ INSERT INTO Test(Registration, TestNo, TestDate, CentreName) VALUES('12-D-12345'
 INSERT INTO Test(Registration, TestNo, TestDate, CentreName) VALUES('06-D-51234', 006, TO_DATE('2012-08-12', 'YYYY-MM-DD'), 'Swords Centre');
 
 --Test Part
-INSERT INTO TestPart(PartName, TestPartPassed, PartComment, MechanincName, TestNo) VALUES();
+INSERT INTO TestPart(PartName, TestPartPassed, PartComment, MechanincName, TestNo) VALUES('Brakes', 1, 'Brakes were in perfect working order', 'Baby Goose', 001 );
+INSERT INTO TestPart(PartName, TestPartPassed, PartComment, MechanincName, TestNo) VALUES('Brakes', 1, 'Working perfectly!', 'Baby Goose', 001);
+INSERT INTO TestPart(PartName, TestPartPassed, PartComment, MechanincName, TestNo) VALUES('Engine', 1, 'Working perfectly!', 'Jackie Chan', 001);
+INSERT INTO TestPart(PartName, TestPartPassed, PartComment, MechanincName, TestNo) VALUES('Chasis', 1, 'Working perfectly!', 'Ivan Ivanson', 001);
+INSERT INTO TestPart(PartName, TestPartPassed, PartComment, MechanincName, TestNo) VALUES('Engine', 1, 'Working perfectly!', 'Jackie Chan', 002);
+INSERT INTO TestPart(PartName, TestPartPassed, PartComment, MechanincName, TestNo) VALUES('Chasis', 1, 'Working perfectly!', 'Ivan Ivanson', 002);
+INSERT INTO TestPart(PartName, TestPartPassed, PartComment, MechanincName, TestNo) VALUES('Brakes', 1, 'Working perfectly!', 'Baby Goose', 003);
+INSERT INTO TestPart(PartName, TestPartPassed, PartComment, MechanincName, TestNo) VALUES('Engine', 0, 'Engine consistently misfiring', 'Jackie Chan', 003);
+INSERT INTO TestPart(PartName, TestPartPassed, PartComment, MechanincName, TestNo) VALUES('Chasis', 0, 'Left side of Chasis is bent!', 'Ivan Ivanson', 003);
+INSERT INTO TestPart(PartName, TestPartPassed, PartComment, MechanincName, TestNo) VALUES('Brakes', 1, 'Working perfectly!', 'Bertie Ahern', 004);
+INSERT INTO TestPart(PartName, TestPartPassed, PartComment, MechanincName, TestNo) VALUES('Engine', 1, 'Working perfectly!', 'Mark Wahlberg', 004);
+INSERT INTO TestPart(PartName, TestPartPassed, PartComment, MechanincName, TestNo) VALUES('Chasis', 1, 'Working perfectly!', 'Bruce Billson', 004);
+INSERT INTO TestPart(PartName, TestPartPassed, PartComment, MechanincName, TestNo) VALUES('Interior', 0, 'Seats need of replacement!', 'Jimmy Cricket', 004);
+INSERT INTO TestPart(PartName, TestPartPassed, PartComment, MechanincName, TestNo) VALUES('Brakes', 0, 'Brake pads slightly worn', 'Bertie Ahern', 005);
+INSERT INTO TestPart(PartName, TestPartPassed, PartComment, MechanincName, TestNo) VALUES('Engine', 1, 'Working perfectly!', 'Mark Wahlberg', 005);
+INSERT INTO TestPart(PartName, TestPartPassed, PartComment, MechanincName, TestNo) VALUES('Chasis', 1, 'Working perfectly!', 'Bruce Billson', 005);
+INSERT INTO TestPart(PartName, TestPartPassed, PartComment, MechanincName, TestNo) VALUES('Interior', 1, 'Lovely Interior!', 'Jimmy Cricket', 005);
 
+
+--Criticality
+INSERT INTO Criticality(CriticalityLevel, PartName, Test_No.) VALUES ('High', 'Brakes', 002);
+INSERT INTO Criticality(CriticalityLevel, PartName, Test_No.) VALUES ('High', 'Engine', 003);
+INSERT INTO Criticality(CriticalityLevel, PartName, Test_No.) VALUES ('High', 'Chasis', 003);
+INSERT INTO Criticality(CriticalityLevel, PartName, Test_No.) VALUES ('Low', 'Interior', 004);
+INSERT INTO Criticality(CriticalityLevel, PartName, Test_No.) VALUES ('High', 'Gearbox', 005);
+INSERT INTO Criticality(CriticalityLevel, PartName, Test_No.) VALUES ('High', 'Mirrors', 005);
+INSERT INTO Criticality(CriticalityLevel, PartName, Test_No.) VALUES ('Medium', 'Door locks', 005);
+INSERT INTO Criticality(CriticalityLevel, PartName, Test_No.) VALUES ('High', 'Seat belts', 002);
+INSERT INTO Criticality(CriticalityLevel, PartName, Test_No.) VALUES ('High', 'Gearbox', 002);
