e89f060df711f8abc37cfcf72195848fca213b4e
unknown <shanefanjo@gmail.com>
2014-10-23 14:41:35 +0100

    Insert statements for Owner

---
+++ LettersData.sql
@@ -0,0 +1,2 @@
+INSERT INTO Letters (Registration, Date, ClerkName, Name, Address, OwnerID)
+VALUES ('09-D-01234', '01-jan-2013', 'John Smith', 'Leslie Knope', '10 Birdwalk Park', '104')
---
+++ OwnerData.sql
@@ -0,0 +1,14 @@
+INSERT INTO Owner (OwnerID, Name, Address)
+VALUES (100, 'Mark Flanagan', '123 Fake Street');
+INSERT INTO Owner (OwnerID, Name, Address)
+VALUES (101, 'Sean Boomington', '108 Johnstown Avenue');
+INSERT INTO Owner (OwnerID, Name, Address)
+VALUES (102, 'Shane Fanning', '102 Lad Lane');
+INSERT INTO Owner (OwnerID, Name, Address)
+VALUES (103, 'Ron Swanson', '56 Unknown Road');
+INSERT INTO Owner (OwnerID, Name, Address)
+VALUES (104, 'Leslie Knope', '10 Birdwalk Park');
+INSERT INTO Owner (OwnerID, Name, Address)
+VALUES (105, 'Tom Dwan', '37 Cypress Hill');
+INSERT INTO Owner (OwnerID, Name, Address)
+VALUES (106, 'Antonio Esfandiari', '40 Green Street');
